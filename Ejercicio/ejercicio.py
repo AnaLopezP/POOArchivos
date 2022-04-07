29578fadfba9f42bb2f37d23f92f6c3af528beaa
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-07 21:13:54 +0200

    prueba 4

--- Ejercicio/ejercicio.py
+++ Ejercicio/ejercicio.py
@@ -6,7 +6,9 @@ def calif_to_lista():
     with open ('calificaciones.csv', encoding = 'UTF-8') as file:
         delimitador = ';'
         leer = csv.DictReader(file, delimitador)
-        sort = sorted(leer, key = operator.itemgetter('Apellidos'))
+        for i in leer:
+            print(i)
+        sort = sorted(leer, key = operator.itemgetter(1))
         for i in sort:
             info_alumnos.append(i)
     return info_alumnos
