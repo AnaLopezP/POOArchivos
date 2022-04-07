4453e1f8ff81253e512f25e3b83b199a240bd035
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-07 20:37:12 +0200

    prueba lectura csv

--- Ejercicio/ejercicio.py
+++ Ejercicio/ejercicio.py
@@ -1,7 +1,17 @@
+import csv
+
 #estructura general:
 def calif_to_lista():
-    pass
+    open ('calificaciones.csv')
+    leer = csv.reader('calificaciones.csv')
+    for i in leer:
+        print(i)
+
+
+
 def añadir_final():
     pass
 def aprobados_suspensos():
     pass
+
+print(calif_to_lista())
