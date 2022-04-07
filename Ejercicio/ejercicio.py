d28b7ee500142585e456df093aeafdae84d6fe07
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-07 20:57:19 +0200

    prueba 3 lista

--- Ejercicio/ejercicio.py
+++ Ejercicio/ejercicio.py
@@ -3,13 +3,13 @@ import operator
 info_alumnos = []
 #estructura general:
 def calif_to_lista():
-    with open ('calificaciones.csv') as file:
+    with open ('calificaciones.csv', encoding = 'UTF-8') as file:
         delimitador = ';'
         leer = csv.DictReader(file, delimitador)
         sort = sorted(leer, key = operator.itemgetter('Apellidos'))
         for i in sort:
             info_alumnos.append(i)
-        return info_alumnos
+    return info_alumnos
 
 
 
