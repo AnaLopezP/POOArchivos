f73c45b77b24bb0a1e55bb63ca3f6b3a990f1742
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-07 22:08:00 +0200

    intento 1  funcion añadir final

--- Ejercicio/ejercicio.py
+++ Ejercicio/ejercicio.py
@@ -16,9 +16,20 @@ def calif_to_lista():
 
 
 
-def añadir_final():
-    pass
+def añadir_final(lista):
+    for i in lista:
+        nota_parcial1 = i.get('Parcial1')
+        nota_parcial2 = i.get('Parcial2')
+        nota_practicas = i.get('Practicas')
+        i['Final'] = float(nota_parcial1)*0.3 + float(nota_parcial2)*0.3 + float(nota_practicas)*0.4
+
+
+
+
+
+
 def aprobados_suspensos():
     pass
 
 calif_to_lista()
+añadir_final(info_alumnos)
